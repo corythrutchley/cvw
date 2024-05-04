cc885ce12a4b487659d3486af26b5573cbfdbbea
Cory Thrutchley <cory.thrutchley@okstate.edu>
2024-05-03 20:41:02 -0500

    cacheRand file added

--- src/cache/cacheRand.sv
+++ src/cache/cacheRand.sv
@@ -28,49 +28,40 @@
 // and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-module cacheLRU
+module cacheRand
   #(parameter NUMWAYS = 4, SETLEN = 9, OFFSETLEN = 5, NUMLINES = 128) (
   input  logic                clk, 
   input  logic                reset,
   input  logic                FlushStage,
-  input  logic                CacheEn,         // Enable the cache memory arrays.  Disable hold read data constant
-  input  logic [NUMWAYS-1:0]  HitWay,          // Which way is valid and matches PAdr's tag
   input  logic [NUMWAYS-1:0]  ValidWay,        // Which ways for a particular set are valid, ignores tag
   input  logic [SETLEN-1:0]   CacheSetData,    // Cache address, the output of the address select mux, NextAdr, PAdr, or FlushAdr
-  input  logic [SETLEN-1:0]   CacheSetTag,     // Cache address, the output of the address select mux, NextAdr, PAdr, or FlushAdr
-  input  logic [SETLEN-1:0]   PAdr,            // Physical address 
-  input  logic                LRUWriteEn,      // Update the LRU state
-  input  logic                SetValid,        // Set the dirty bit in the selected way and set
+  input  logic                WriteEn,         // Update the LRU state
   input  logic                ClearValid,      // Clear the dirty bit in the selected way and set
   input  logic                InvalidateCache, // Clear all valid bits
   output logic [NUMWAYS-1:0]  VictimWay        // LRU selects a victim to evict
 );
 
   localparam                           LOGNUMWAYS = $clog2(NUMWAYS);
+  logic [NUMWAYS-2:0]                  Memory [NUMLINES-1:0];
 
-  logic [NUMWAYS-2:0]                  LRUMemory [NUMLINES-1:0];
-  logic [NUMWAYS-2:0]                  CurrLRU;
-  logic [NUMWAYS-2:0]                  NextLRU;
+  // logic [NUMWAYS-2:0]                  Curr;
+  logic [6:0]                          Curr;
+  // logic                                Curr0, Curr1, Curr2, Curr3, Curr4, Curr5, Curr6;
+  logic                                ShiftIn;
+  logic [NUMWAYS-2:0]                  Next;
   logic [LOGNUMWAYS-1:0]               HitWayEncoded, Way;
-  logic [NUMWAYS-2:0]                  WayExpanded;
   logic                                AllValid;
-  
   genvar                               row;
-
+  logic                                en;
   /* verilator lint_off UNOPTFLAT */
   // Rose: For some reason verilator does not like this.  I checked and it is not a circular path.
-  logic [NUMWAYS-2:0]                  LRUUpdate;
   logic [LOGNUMWAYS-1:0] Intermediate [NUMWAYS-2:0];
   /* verilator lint_on UNOPTFLAT */
-
   logic [NUMWAYS-1:0] FirstZero;
   logic [LOGNUMWAYS-1:0] FirstZeroWay;
   logic [LOGNUMWAYS-1:0] VictimWayEnc;
-
   binencoder #(NUMWAYS) hitwayencoder(HitWay, HitWayEncoded);
-
   assign AllValid = &ValidWay;
-
   ///// Update replacement bits.
   // coverage off
   // Excluded from coverage b/c it is untestable without varying NUMWAYS.
@@ -82,21 +73,7 @@ module cacheLRU
     return log2;
   endfunction // log2
   // coverage on
-
-  // On a miss we need to ignore HitWay and derive the new replacement bits with the VictimWay.
-  mux2 #(LOGNUMWAYS) WayMuxEnc(HitWayEncoded, VictimWayEnc, SetValid, Way);
-
-  // bit duplication
-  // expand HitWay as HitWay[3], {{2}{HitWay[2]}}, {{4}{HitWay[1]}, {{8{HitWay[0]}}, ...
-  for(row = 0; row < LOGNUMWAYS; row++) begin
-    localparam integer DuplicationFactor = 2**(LOGNUMWAYS-row-1);
-    localparam StartIndex = NUMWAYS-2 - DuplicationFactor + 1;
-    localparam EndIndex = NUMWAYS-2 - 2 * DuplicationFactor + 2;
-    assign WayExpanded[StartIndex : EndIndex] = {{DuplicationFactor}{Way[row]}};
-  end
-
   genvar               node;
-  assign LRUUpdate[NUMWAYS-2] = '1;
   for(node = NUMWAYS-2; node >= NUMWAYS/2; node--) begin : enables
     localparam ctr = NUMWAYS - node - 1;
     localparam ctr_depth = log2(ctr);
@@ -106,52 +83,45 @@ module cacheLRU
 
     // the child node will be updated if its parent was updated and
     // the Way bit was the correct value.
-    // The if statement is only there for coverage since LRUUpdate[root] is always 1.
-    if (node == NUMWAYS-2) begin
-      assign LRUUpdate[lchild] = ~Way[r];
-      assign LRUUpdate[rchild] = Way[r];
-    end else begin
-      assign LRUUpdate[lchild] = LRUUpdate[node] & ~Way[r];
-      assign LRUUpdate[rchild] = LRUUpdate[node] & Way[r];
-    end
   end
 
-  // The root node of the LRU tree will always be selected in LRUUpdate. No mux needed.
-  assign NextLRU[NUMWAYS-2] = ~WayExpanded[NUMWAYS-2];
-  if (NUMWAYS > 2) mux2 #(1) LRUMuxes[NUMWAYS-3:0](CurrLRU[NUMWAYS-3:0], ~WayExpanded[NUMWAYS-3:0], LRUUpdate[NUMWAYS-3:0], NextLRU[NUMWAYS-3:0]);
-
-  // Compute next victim way.
-  for(node = NUMWAYS-2; node >= NUMWAYS/2; node--) begin
-    localparam t0 = 2*node - NUMWAYS;
-    localparam t1 = t0 + 1;
-    assign Intermediate[node] = CurrLRU[node] ? Intermediate[t0] : Intermediate[t1];
-  end
-  for(node = NUMWAYS/2-1; node >= 0; node--) begin
-    localparam int0 = (NUMWAYS/2-1-node)*2;
-    localparam int1 = int0 + 1;
-    assign Intermediate[node] = CurrLRU[node] ? int1[LOGNUMWAYS-1:0] : int0[LOGNUMWAYS-1:0];
-  end
-
-  
   priorityonehot #(NUMWAYS) FirstZeroEncoder(~ValidWay, FirstZero);
   binencoder #(NUMWAYS) FirstZeroWayEncoder(FirstZero, FirstZeroWay);
-  mux2 #(LOGNUMWAYS) VictimMux(FirstZeroWay, Intermediate[NUMWAYS-2], AllValid, VictimWayEnc);
+  mux2 #(LOGNUMWAYS) VictimMux(FirstZeroWay, Curr[1:0], AllValid, VictimWayEnc);
   decoder #(LOGNUMWAYS) decoder (VictimWayEnc, VictimWay);
 
+  flopenr #(7) flop0(clk, reset, en, Curr[1], Curr[0]);
+  flopenr #(7) flop1(clk, reset, en, Curr[2], Curr[1]);
+  flopenr #(7) flop2(clk, reset, en, Curr[3], Curr[2]);
+  flopenr #(7) flop3(clk, reset, en, Curr[4], Curr[3]);
+  flopenr #(7) flop4(clk, reset, en, Curr[5], Curr[4]);
+  flopenr #(7) flop5(clk, reset, en, Curr[6], Curr[5]);
+  flopenr #(7) flop6(clk, reset, en, ShiftIn, Curr[6]);
+
+  assign ShiftIn = Curr6 ^ Curr5 ^ Curr3 ^ Curr0;
+
+  assign en = ~FlushStage & WriteEn;
   // LRU storage must be reset for modelsim to run. However the reset value does not actually matter in practice.
   // This is a two port memory.
   // Every cycle must read from CacheSetData and each load/store must write the new LRU.
+  module flopenr #(parameter WIDTH = 8)
+   (input  logic             clk, reset, en,
+    input logic [WIDTH-1:0]  d, 
+    output logic [WIDTH-1:0] q);
 
-  // note: Verilator lint doesn't like <= for array initialization (https://verilator.org/warn/BLKLOOPINIT?v=5.021)
-  // Move to = to keep Verilator happy and simulator running fast
+   always_ff @(posedge clk, posedge reset)
+     if (reset)   q <= 0;
+     else if (en) q <= d;
+  endmodule
+
   always_ff @(posedge clk) begin
     if (reset | (InvalidateCache & ~FlushStage)) 
-      for (int set = 0; set < NUMLINES; set++) LRUMemory[set] = '0; // exclusion-tag: initialize
+      for (int set = 0; set < NUMLINES; set++) Memory[set] = '0; // exclusion-tag: initialize
     else if(CacheEn) begin
       // Because we are using blocking assignments, change to LRUMemory must occur after LRUMemory is used so we get the proper value
-      if(LRUWriteEn & (PAdr == CacheSetTag)) CurrLRU = NextLRU;
-      else                                   CurrLRU = LRUMemory[CacheSetTag];
-      if(LRUWriteEn)                         LRUMemory[PAdr] = NextLRU;
+      if(WriteEn & (PAdr == CacheSetTag)) Curr = Next;
+      else                                Curr = Memory[CacheSetTag];
+      if(WriteEn)                         Memory[PAdr] = Next;
     end
   end
 
